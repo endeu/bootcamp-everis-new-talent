27d174717c5744e364f6adfb241942c36d9e1288
endeu <wendellpguariento@gmail.com>
2021-11-03 21:48:09 -0300

    first project in Java created rectifed

---
+++ primerio projeto/src/br/com/dio/PrimeiroProjeto.java
@@ -0,0 +1,19 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class PrimeiroProjeto {
+    public static void main(String[] args) {
+
+        gato gato = new gato();
+
+        System.out.println(gato);
+
+        /*
+        int a = 5;
+        int b = 6;
+
+        System.out.printf("Hello World! " + (a+b));
+         */
+    }
+}
---
+++ primerio projeto/src/br/com/dio/model/gato.java
@@ -0,0 +1,70 @@
+package br.com.dio.model;
+
+import java.util.Objects;
+
+public class gato {
+    private String nome;
+    private String cor;
+    private Integer idade;
+
+    //constructor vazio
+    public gato() {
+    }
+
+    //constructor atributos
+    public gato(String nome, String cor, Integer idade) {
+        this.nome = nome;
+        this.cor = cor;
+        this.idade = idade;
+    }
+
+    //getters e setters nome
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    //getters e setters cor
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    //getters e setters idade
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    //equals and hashcode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        gato gato = (gato) o;
+        return Objects.equals(nome, gato.nome) && Objects.equals(cor, gato.cor) && Objects.equals(idade, gato.idade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, cor, idade);
+    }
+
+    @Override
+    public String toString() {
+        return "gato{" +
+                "nome='" + nome + '\'' +
+                ", cor='" + cor + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+}
